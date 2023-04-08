c67b6582102be4f9ae91ab91512f3f09e25c9aef
andmansim <91722324+andmansim@users.noreply.github.com>
2023-04-08 12:17:17 +0200

    inicio, entender problema

---
+++ ejercicio.py
@@ -0,0 +1,8 @@
+'''
+Barbero tiene 3 sillas, atiende a n clientes. No clientes barbero duerme, llega uno este le despierta
+Llega un cliente y el barbero está ocupado, se sienta en la silla 1, si llega otro en la 2 y luego en la 3
+estos esperan en las sillas. 3 sillas ocupadas y barbero también entonces cliente se va 
+
+'''
+class Barbero:
+    #estados del barbero
