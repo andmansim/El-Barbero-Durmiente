66c5f668f3edcad8319ef4c969366dfd43a6a8be
andmansim <91722324+andmansim@users.noreply.github.com>
2023-04-08 12:25:41 +0200

    clase cliente

--- ejercicio.py
+++ ejercicio.py
@@ -14,4 +14,20 @@ class Barbero:
     
     def setter(self, nuevo):
         self.estado = nuevo #cambiamos el estado del barbero
-        
+
+class Cliente:
+    #estados del cliente: esperando en una silla, atendido o se va(no hay sitio para él)
+    #posición si está en la barbería: silla 1, silla 2, silla 3 o barbero
+    #si está siendo atendido, tiene que tener un tiempo (para que le corte el pelo o lo que sea)
+    def __init__(self):
+        self.estado = None #no tiene estado
+        self.lugar = None #donde está el cliente
+    def get_estado(self):
+        return self.estado
+    def get_posicion(self):
+        return self.lugar
+    
+    def set_estado(self, nuevo):
+        self.estado = nuevo
+    def set_posicion(self, nuevo):
+        self.lugar = nuevo
